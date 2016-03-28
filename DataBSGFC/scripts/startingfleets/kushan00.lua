51aeef13ac27923499c3e8403dc59a3594b2927c
Sastrei <sastrei@gmail.com>
2016-03-28 17:55:54 -0500

    Initial
    Initial upload
    - hur_battleraptor
    - hur_raptor
    - hur_vipermkii
    - hur_vipermkvii
    - hur_berzerk
    - hur_defender
    - hur_civ_gemenontraveller
    - hur_civ_therasita

---
+++ DataBSGFC/scripts/startingfleets/kushan00.lua
@@ -0,0 +1,101 @@
+PersistantData = {
+  StrikeGroups = {
+  },
+  Squadrons = {
+    {
+      type = "Kus_MotherShip",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+    },
+    {
+      type = "Kus_ResourceCollector",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 6,
+    },    
+    {
+      type = "Kus_Carrier",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+	{
+      type = "Hur_Battleraptor",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      size = 1,
+      number = 3, 
+    },
+	{
+      type = "Hur_ViperMkII",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      size = 1,
+      number = 3, 
+    },
+	{
+      type = "Hur_ViperMkVII",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      size = 1,
+      number = 3, 
+    },
+	{
+      type = "Hur_Raptor",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      size = 1,
+      number = 3, 
+    },
+	{
+      type = "Hur_Berzerk",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+	{
+      type = "Hur_Defender",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+	{
+      type = "Hur_Civ_TheraSita",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+	{
+      type = "Hur_Civ_GemenonTraveller",
+      subsystems = {},
+      shiphold = {},
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+  },
+Research = {
+  },
+}
